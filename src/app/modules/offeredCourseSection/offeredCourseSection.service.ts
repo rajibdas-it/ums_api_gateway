770377cb180daefe28ae8ac0da3ba537455a3fab
rajibdas-it <rajibrad@gmail.com>
2023-11-12 17:07:17 +0600

    done

--- src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
+++ src/app/modules/offeredCourseSection/offeredCourseSection.service.ts
@@ -1,7 +1,16 @@
 import { OfferedCourseSection } from '@prisma/client';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import prisma from '../../../shared/prisma';
 
 const createOfferedCourseSection = async (data: OfferedCourseSection) => {
+  const isExist = await prisma.offeredCourse.findFirst({
+    where: { id: data.offeredCourseId },
+  });
+  if (!isExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Offered course not found');
+  }
+
   const result = await prisma.offeredCourseSection.create({
     data,
   });
